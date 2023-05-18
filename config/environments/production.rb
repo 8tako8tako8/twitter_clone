363bb9f43d56a5b84900bcd9e8ac4a02518bf5ec
森本晃輔 <8tako8tako8@gmail.com>
2023-05-19 04:18:41 +0900

    本番環境でmailerのhostを設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'evening-beach-02976.herokuapp.com' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = { user_name: 'apikey', password: ENV['SENDGRID_API_KEY'],
