b490f920344e434c812e427441b9f0562c244aff
森本晃輔 <8tako8tako8@gmail.com>
2023-05-19 03:23:23 +0900

    User model：deviseの全てのmoduleを有効化。バリデーションを追加。

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable,
+        :confirmable, :lockable, :timeoutable, :omniauthable
+
+  validates :tel, presence: true, uniqueness: true
+  validates :user_name, presence: true, uniqueness: true
+  validates :birthdate, presence: true
+end
