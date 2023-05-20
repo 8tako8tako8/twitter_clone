e4da7bac7a3d0f8347180965631ca92313c51c05
森本晃輔 <8tako8tako8@gmail.com>
2023-05-21 03:19:01 +0900

    GitHub認証時にユーザー登録する

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,17 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :timeoutable, :omniauthable
+         :confirmable, :lockable, :timeoutable, :omniauthable, omniauth_providers: %i[github]
 
-  validates :tel, presence: true, uniqueness: true
-  validates :user_name, presence: true, uniqueness: true
+  validates :tel, presence: true
+  validates :user_name, presence: true
   validates :birthdate, presence: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.confirmed_at = Time.now
+    end
+  end
 end
