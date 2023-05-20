a247bc06c3d79e637a09b2193868e0c4b59f2ecb
森本晃輔 <8tako8tako8@gmail.com>
2023-05-21 03:19:33 +0900

    GitHub認証時にuser_name,birthdate,telのバリデーションをスキップする

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable, :omniauthable, omniauth_providers: %i[github]
 
-  validates :tel, presence: true
-  validates :user_name, presence: true
-  validates :birthdate, presence: true
+  validates :tel, presence: true, unless: :github_provider?
+  validates :user_name, presence: true, unless: :github_provider?
+  validates :birthdate, presence: true, unless: :github_provider?
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -15,5 +15,11 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
       user.confirmed_at = Time.now
     end
+  end
+
+  private
+
+  def github_provider?
+    self.provider == "github"
   end
 end
