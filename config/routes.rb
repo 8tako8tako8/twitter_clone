27975156433381de4e04b067d8c7dc002627cf45
森本晃輔 <8tako8tako8@gmail.com>
2023-05-21 03:23:04 +0900

    routesにコールバックのパスを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'tweets#index'
 end
