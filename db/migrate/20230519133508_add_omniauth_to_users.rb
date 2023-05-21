54499f12d23f5447dea3b645db8b6dbe1653daf6
森本晃輔 <8tako8tako8@gmail.com>
2023-05-21 21:25:05 +0900

    マイグレーションファイルを元に戻す

--- db/migrate/20230519133508_add_omniauth_to_users.rb
+++ db/migrate/20230519133508_add_omniauth_to_users.rb
@@ -2,9 +2,7 @@
 
 class AddOmniauthToUsers < ActiveRecord::Migration[7.0]
   change_table :users, bulk: true do |t|
-    t.string :provider, null: false, default: ''
-    t.string :uid, null: false
-
-    t.index %i[provider uid], unique: true
+    t.string :provider
+    t.string :uid
   end
 end
