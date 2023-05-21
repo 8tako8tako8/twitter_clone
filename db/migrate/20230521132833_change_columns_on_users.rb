81a4a9474239bced2664124de875d04f87e188e9
森本晃輔 <8tako8tako8@gmail.com>
2023-05-21 22:40:52 +0900

    RuboCop

--- db/migrate/20230521132833_change_columns_on_users.rb
+++ db/migrate/20230521132833_change_columns_on_users.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class ChangeColumnsOnUsers < ActiveRecord::Migration[7.0]
   def change
-    change_column_default :users, :user_name, from: nil, to: ""
-    change_column_default :users, :tel, from: nil, to: ""
+    change_column_default :users, :user_name, from: nil, to: ''
+    change_column_default :users, :tel, from: nil, to: ''
     change_column_null :users, :birthdate, true
 
     remove_index :users, :user_name if index_exists?(:users, :user_name)
