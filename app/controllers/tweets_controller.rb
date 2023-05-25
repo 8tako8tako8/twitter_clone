bbe4f852be83f69e2b8b14883e28ef2f0aa0a09c
森本晃輔 <8tako8tako8@gmail.com>
2023-05-26 05:06:46 +0900

    controller: Tweets#indexを追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class TweetsController < ApplicationController
-  def index; end
+  def index
+    case params[:view]
+    when 'follow'
+      # フォローしているユーザーのツイート一覧
+      followed_user_ids = current_user.followings.pluck(:followed_user_id)
+      @tweets = Tweet.where(user_id: followed_user_ids).order(created_at: :desc).page(params[:page]).per(10)
+    else
+      # 全てのツイート一覧
+      @tweets = Tweet.all.order(created_at: :desc).page(params[:page]).per(10)
+    end
+  end
 end
