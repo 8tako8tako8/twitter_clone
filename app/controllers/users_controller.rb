abfa040ba4ba8a62bcb909cf5917280b2055c3bf
森本晃輔 <8tako8tako8@gmail.com>
2023-05-28 04:03:12 +0900

    controller: Users#showを追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def show
+    return redirect root_path if !user_signed_in?
+
+    @user = User.find(params[:id])
+    case params[:view]
+    when 'favorite'
+      @favorites = Favorite.where(user_id: params[:id]).order(created_at: :desc).page(params[:page]).per(10)
+    when 'retweet'
+      @retweets = Retweet.where(user_id: params[:id]).order(created_at: :desc).page(params[:page]).per(10)
+    when 'comment'
+      @comments = Comment.where(user_id: params[:id]).order(created_at: :desc).page(params[:page]).per(10)
+    else
+      @tweets = Tweet.where(user_id: params[:id]).order(created_at: :desc).page(params[:page]).per(10)
+    end
+  end
+end
