2361a388c4abdb99012b63510e67e3ff6b1d53c3
森本晃輔 <8tako8tako8@gmail.com>
2023-05-29 05:19:54 +0900

    controller: Users#indexを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,6 @@ class UsersController < ApplicationController
     return redirect_to root_path unless user_signed_in?
 
     @user = User.find(params[:id])
-    case params[:view]
-    when 'favorite'
-      @favorites = Favorite.where(user_id: params[:id]).order(created_at: :desc).page(params[:page]).per(10)
-    when 'retweet'
-      @retweets = Retweet.where(user_id: params[:id]).order(created_at: :desc).page(params[:page]).per(10)
-    when 'comment'
-      @comments = Comment.where(user_id: params[:id]).order(created_at: :desc).page(params[:page]).per(10)
-    else
-      @tweets = Tweet.where(user_id: params[:id]).order(created_at: :desc).page(params[:page]).per(10)
-    end
+    @tweets = @user.tweets.order(created_at: :desc).page(params[:page]).per(10)
   end
 end
