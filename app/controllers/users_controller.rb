265953d07ea44d9c933fed16d697b5d684d1aeba
森本晃輔 <8tako8tako8@gmail.com>
2023-05-28 21:34:11 +0900

    redirect_toに修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    return redirect root_path if !user_signed_in?
+    return redirect_to root_path if !user_signed_in?
 
     @user = User.find(params[:id])
     case params[:view]
