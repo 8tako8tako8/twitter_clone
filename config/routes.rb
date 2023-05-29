ec4933dc2958b96009b6572bc7d3b4ad78439dd3
森本晃輔 <8tako8tako8@gmail.com>
2023-05-30 05:05:07 +0900

    routes: Users#edit, updateを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
-  resources :users, only: [:show]
+  resources :users, only: %i[show edit update]
   root 'tweets#index'
 end
