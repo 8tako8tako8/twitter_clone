952aa8e8299e61659fee9aa08dff6f2230f2f75b
森本晃輔 <8tako8tako8@gmail.com>
2023-05-29 15:36:23 +0900

    ログインチェックをbefore_actionで行う

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    return redirect_to root_path unless user_signed_in?
     return redirect_to root_path if params[:user_id].nil?
 
     @user = User.find(params[:user_id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def show
-    return redirect_to root_path unless user_signed_in?
+  before_action :authenticate_user!
 
+  def show
     @user = User.find(params[:id])
     @tweets = @user.tweets.order(created_at: :desc).page(params[:page]).per(10)
   end
