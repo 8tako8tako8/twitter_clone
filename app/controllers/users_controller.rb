d3ca0f8daeb7091d506d064812e3496fa133e28f
森本晃輔 <8tako8tako8@gmail.com>
2023-05-30 05:01:25 +0900

    controller: Users#edit, updateを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,24 @@ class UsersController < ApplicationController
       @tweets = Tweet.where(user_id: params[:id]).order(created_at: :desc).page(params[:page]).per(10)
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = '変更を保存しました。'
+      redirect_to user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:header_image, :image, :introduction, :location, :website_url, :birthdate)
+  end
 end
