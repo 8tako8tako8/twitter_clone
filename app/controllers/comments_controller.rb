1162edf4467d8cc0180fcb9490865656407ca700
森本晃輔 <8tako8tako8@gmail.com>
2023-05-30 07:10:06 +0900

    controller: Comments#createを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,4 +7,15 @@ class CommentsController < ApplicationController
     @user = User.find(params[:user_id])
     @comments = @user.comments.order(created_at: :desc).page(params[:page]).per(10)
   end
+
+  def create
+    tweet = Tweet.find(params[:id])
+    current_user.comment(comment_params, tweet)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
 end
