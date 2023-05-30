61b4431a7e5ff73e04a3f23d844cb5939750d16b
森本晃輔 <8tako8tako8@gmail.com>
2023-05-30 16:07:49 +0900

    RuboCop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,9 +30,9 @@ class UsersController < ApplicationController
   end
 
   def identical_user
-    unless current_user.id == params[:id].to_i
-      flash[:alert] = "アクセス権がありません。"
-      redirect_to root_path
-    end
+    return if current_user.id == params[:id].to_i
+
+    flash[:alert] = 'アクセス権がありません。'
+    redirect_to root_path
   end
 end
