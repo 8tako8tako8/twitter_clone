b983c79fed8be632ac9e2463df23800de5a886cf
森本晃輔 <8tako8tako8@gmail.com>
2023-05-30 16:31:30 +0900

    RuboCop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,7 @@ class UsersController < ApplicationController
     @tweets = @user.tweets.order(created_at: :desc).page(params[:page]).per(10)
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if current_user.update(user_params)
