3e839fd7f9dd5b100ff467d15c713486ab1f8cd7
森本晃輔 <8tako8tako8@gmail.com>
2023-05-30 16:23:42 +0900

    current_userをupdateするように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@
 
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :identical_user, only: %i[edit update]
 
   def show
     @user = User.find(params[:id])
@@ -14,10 +13,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
+    if current_user.update(user_params)
       flash[:notice] = '変更を保存しました。'
-      redirect_to user_path(@user)
+      redirect_to user_path(current_user)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -27,12 +25,5 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:header_image, :image, :introduction, :location, :website_url, :birthdate)
-  end
-
-  def identical_user
-    return if current_user.id == params[:id].to_i
-
-    flash[:alert] = 'アクセス権がありません。'
-    redirect_to root_path
   end
 end
