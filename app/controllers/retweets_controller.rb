4bd5c73160562ca0f4b5686f690bc770039ca6cf
森本晃輔 <8tako8tako8@gmail.com>
2023-06-03 03:20:20 +0900

    controller: Retweets#create, destroyを追加

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -7,4 +7,17 @@ class RetweetsController < ApplicationController
     @user = User.find(params[:user_id])
     @retweets = @user.retweets.order(created_at: :desc).page(params[:page]).per(10)
   end
+
+  def create
+    tweet = Tweet.find(params[:tweet_id])
+    current_user.retweet(tweet)
+    redirect_to request.referer || root_path
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:tweet_id])
+    retweet = current_user.retweets.find_by(tweet_id: tweet.id)
+    retweet.destroy
+    redirect_to request.referer || root_path
+  end
 end
