f59a5061fe95819428967ba3ffc855e1ef29ac01
森本晃輔 <8tako8tako8@gmail.com>
2023-06-03 01:34:03 +0900

    controller: favorites#create,destroyを追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,4 +7,17 @@ class FavoritesController < ApplicationController
     @user = User.find(params[:user_id])
     @favorites = @user.favorites.order(created_at: :desc).page(params[:page]).per(10)
   end
+
+  def create
+    tweet = Tweet.find(params[:tweet_id])
+    favorite = current_user.favorite(tweet)
+    redirect_to request.referrer || root_path
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:tweet_id])
+    favorite = current_user.favorites.find_by(tweet_id: tweet.id)
+    favorite.destroy
+    redirect_to request.referrer || root_path
+  end
 end
