d2ac0eb0f90690d63d9d71adc2e4a17e67e8b0d3
森本晃輔 <8tako8tako8@gmail.com>
2023-06-03 01:46:16 +0900

    RuboCop

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,14 +10,14 @@ class FavoritesController < ApplicationController
 
   def create
     tweet = Tweet.find(params[:tweet_id])
-    favorite = current_user.favorite(tweet)
-    redirect_to request.referrer || root_path
+    current_user.favorite(tweet)
+    redirect_to request.referer || root_path
   end
 
   def destroy
     tweet = Tweet.find(params[:tweet_id])
     favorite = current_user.favorites.find_by(tweet_id: tweet.id)
     favorite.destroy
-    redirect_to request.referrer || root_path
+    redirect_to request.referer || root_path
   end
 end
