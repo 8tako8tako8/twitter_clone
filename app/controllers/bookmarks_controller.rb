7ddab6d20450914afcd4eb357c72cf20686579eb
森本晃輔 <8tako8tako8@gmail.com>
2023-06-04 03:41:12 +0900

    controller: Bookmarks#index, create, destroyを追加

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,20 @@
+class BookmarksController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @user = User.find(params[:user_id])
+    @bookmarks = @user.bookmarks.order(created_at: :desc).page(params[:page]).per(10)
+  end
+
+  def create
+    tweet = Tweet.find(params[:tweet_id])
+    current_user.bookmark(tweet)
+    redirect_to request.referer || root_path
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:tweet_id])
+    current_user.unbookmark(tweet)
+    redirect_to request.referer || root_path
+  end
+end
