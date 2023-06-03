3192c67e5d41b83dabb7a12fbb7683adb19a7898
森本晃輔 <8tako8tako8@gmail.com>
2023-06-04 08:50:34 +0900

    場合分けしてflashメッセージを表示する

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -5,15 +5,25 @@ class FollowsController < ApplicationController
 
   def create
     user = User.find(params[:user_id])
-    current_user.follow(user)
-    flash[:notice] = 'フォローしました'
+    if current_user.id == user.id
+      flash[:alert] = '自分自身をフォローできません'
+    elsif current_user.followings.exists?(user.id)
+      flash[:alert] = 'フォロー済みです'
+    elsif current_user.follow(user)
+      flash[:notice] = 'フォローしました'
+    else
+      flash[:alert] = 'フォローに失敗しました'
+    end
     redirect_to request.referer || root_path
   end
 
   def destroy
     user = User.find(params[:user_id])
-    current_user.unfollow(user)
-    flash[:notice] = 'フォロー解除しました'
+    if current_user.unfollow(user)
+      flash[:notice] = 'フォロー解除しました'
+    else
+      flash[:alert] = 'フォロー解除に失敗しました'
+    end
     redirect_to request.referer || root_path
   end
 end
