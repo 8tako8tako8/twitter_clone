11a9f79855032d785b5b8c46b7dcb597c7995c58
森本晃輔 <8tako8tako8@gmail.com>
2023-06-11 03:53:52 +0900

    controller: Rooms#index, create, showを追加

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class RoomsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.joins(:entries).where(entries: { room: current_user.rooms }).where.not(id: current_user.id).page(params[:page]).per(10)
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    current_user.create_room(user)
+    redirect_to user_room_path(user)
+  end
+
+  def show
+    @users = User.joins(:entries).where(entries: { room: current_user.rooms }).where.not(id: current_user.id).page(params[:page]).per(10)
+    @user = User.find(params[:user_id])
+    @room = current_user.common_room(@user)
+    @message = Message.new
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:user_id, :room_id)
+  end
+end
