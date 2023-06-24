2d9c8e8f9dbba65cd630873a8a9f8034330936fb
森本晃輔 <8tako8tako8@gmail.com>
2023-06-25 02:57:00 +0900

    Bookmarkのモデルスペックを追加

---
+++ spec/models/bookmark_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Bookmark, type: :model do
+  describe 'バリデーション' do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:tweet) { FactoryBot.create(:tweet) }
+
+    it 'ブックマークが正しく作成されること' do
+      bookmark = FactoryBot.build(:bookmark)
+      expect(bookmark).to be_valid
+    end
+
+    it '同一ユーザーが同一ツイートを複数回ブックマークできないこと' do
+      FactoryBot.create(:bookmark, user: user, tweet: tweet)
+      duplicate_bookmark = FactoryBot.build(:bookmark, user: user, tweet: tweet)
+      expect(duplicate_bookmark).not_to be_valid
+    end
+  end
+end
