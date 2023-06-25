01b60ead3195ebb0303536d4650f02d33d3f4427
森本晃輔 <8tako8tako8@gmail.com>
2023-06-25 22:45:27 +0900

    ブックマークのシステムスペックを追加

---
+++ spec/system/bookmarks_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe "Bookmarks", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  it 'ユーザーがブックマークする' do
+    user = FactoryBot.create(:user)
+    tweet = FactoryBot.create(:tweet, tweet: 'ブックマーク用')
+
+    visit root_path
+    click_link 'ログイン'
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button 'ログインする'
+
+    click_link 'ブックマーク用'
+
+    expect(page).not_to have_css('.bi.bi-bookmark.text-primary')
+
+    find('#bookmark-link').click
+
+    expect(page).to have_css('.bi.bi-bookmark.text-primary')
+  end
+
+  it 'ユーザーがブックマーク解除する' do
+    user = FactoryBot.create(:user)
+    tweet = FactoryBot.create(:tweet, tweet: 'ブックマーク用')
+    user.bookmark(tweet)
+
+    visit root_path
+    click_link 'ログイン'
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button 'ログインする'
+
+    click_link 'ブックマーク用'
+
+    expect(page).to have_css('.bi.bi-bookmark.text-primary')
+
+    find('#bookmark-link').click
+
+    expect(page).not_to have_css('.bi.bi-bookmark.text-primary')
+  end
+end
